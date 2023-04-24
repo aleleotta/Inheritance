449960d32a7da88525152ce967fe508bc401dc50
aleleotta <alessandro.leotta@iesnervion.es>
2023-04-24 09:16:58 +0200

    .

--- Inheritance and Interfaces/src/exercise2/Employee.java
+++ Inheritance and Interfaces/src/exercise2/Employee.java
@@ -2,6 +2,26 @@ package exercise2;
 
 public class Employee {
 	private String name = "";
+	
+	public Employee() {}
+	
+	public Employee(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Name: " + name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
 
 /*
