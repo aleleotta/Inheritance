30c328a33990c29dd35264af71eb1ca67e0926bf
Alessandro Leotta <Alessandro Leotta@HPaleleotta>
2023-05-04 22:26:15 +0200

    .

--- Inheritance and Interfaces/src/exercise4/Television.java
+++ Inheritance and Interfaces/src/exercise4/Television.java
@@ -5,4 +5,16 @@ public class Television extends Electronic {
 	private boolean tuner = false;
 	
 	public Television() {}
+	
+	public Television(double price, double weight) {
+		super(price, weight);
+		this.weight = weight;
+	}
+	
+	public Television(double price, double weight, char energyConsumption, int resolution, boolean tuner) {
+		super(price, weight, energyConsumption);
+		this.resolution = resolution;
+		this.tuner = tuner;
+	}
+	
 }
